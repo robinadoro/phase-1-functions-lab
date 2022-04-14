335cab512c43a2c410c25747f40800e0aafa60db
Robinson Idagiza <robinadoro@gmail.com>
2022-04-14 12:53:40 +0300

    ft:Add a function to return distance in block

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks(){
+    return 1;
+
+    // return 8
+}
+distanceFromHqInBlocks(43)
+// distanceFromHqInBlocks(50)
+
+// function distanceFromHqInFeet(){
+//     return 2112
+// }
+// distanceFromHqInFeet(50)
+
+// function distanceTravelledInFeet(){
+//     // return (48-43)*264
+//     // return (60-50)*264
+//     return (34-28)*264
+
+// }
+// distanceTravelledInFeet()
+
+// // function calculatesFarePrice(start, destination)
+// function calculatesFarePrice(start, destination){
+//     // return 0;
+// //  return (((34-32)*264)-400)*0.02
+// // if (feet>=400 && feet <= 2000)
+// // return (((34-32)*264)-400)*0.02
+//     // return 25;
+//     return 'cannot travel that far';
+
+// }
+// // calculatesFarePrice(43, 44);
+// // calculatesFarePrice(34, 32)
+// // calculatesFarePrice(50, 58)
+// calculatesFarePrice(34, 24);
+
